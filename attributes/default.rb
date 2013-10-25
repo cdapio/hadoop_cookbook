a1b6eaebba8a156efac5a204907d9247e01b3605
Chris Gianelloni <chris@continuuity.com>
2013-10-25 16:27:28 -0700

    Default core-site.xml fs.defaultFS to hdfs://localhost

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,8 @@
 default['hadoop']['distribution'] = 'hdp'
 # Default: conf.chef
 default['hadoop']['conf_dir'] = 'conf.chef'
+
+###
+# core-site.xml settings
+###
+default['hadoop']['core_site']['fs.defaultFS'] = 'hdfs://localhost'
