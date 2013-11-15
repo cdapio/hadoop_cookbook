4faaf6ce8e886a74c3ab6578ddb7d41bbde13294
Chris Gianelloni <chris@continuuity.com>
2013-11-15 14:39:18 -0800

    Set attributes for hbase-policy.xml

---
+++ attributes/hbase_policy.rb
@@ -0,0 +1,4 @@
+# These are the defaults for hbase-policy.xml
+default['hbase']['hbase_policy']['security.client.protocol.acl'] = '*'
+default['hbase']['hbase_policy']['security.admin.protocol.acl'] = '*'
+default['hbase']['hbase_policy']['security.masterregion.protocol.acl'] = '*'
