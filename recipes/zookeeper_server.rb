e4e805393f5d8c8bf9b867efda50f58a15bb2131
Chris Gianelloni <chris@continuuity.com>
2013-11-15 12:24:07 -0800

    Initial zookeeper_server recipe to configure and setup a Zookeeper server

---
+++ recipes/zookeeper_server.rb
@@ -0,0 +1,80 @@
+#
+# Cookbook Name:: hadoop
+# Recipe:: zookeeper_server
+#
+# Copyright (C) 2013 Continuuity, Inc.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'hadoop::repo'
+
+package "hadoop-zookeeper-server" do
+  action :install
+end
+
+zookeeper_conf_dir = "/etc/zookeeper/#{node['zookeeper']['conf_dir']}"
+
+directory zookeeper_conf_dir do
+  mode "0755"
+  owner "root"
+  group "root"
+  action :create
+  recursive true
+end
+
+# Setup zoo.cfg
+if node['zookeeper'].has_key? 'zoocfg'
+  myVars = { :properties => node['zookeeper']['zoocfg'] }
+
+  # We need to create a data directory, if it exists
+  if node['zookeeper']['zoocfg'].has_key? 'dataDir'
+    directory node['zookeeper']['zoocfg']['dataDir'] do
+      owner "zookeeper"
+      group "zookeeper"
+      mode "0755"
+      recursive true
+      action :create
+    end
+  end
+
+  template "#{zookeeper_conf_dir}/zoo.cfg" do
+    owner "root"
+    group "root"
+    mode "0644"
+    variables myVars
+    source "generic.properties.erb"
+    action :create
+  end
+end # End zoo.cfg
+
+# Setup log4j.properties
+  if node['hbase'].has_key? 'log4j'
+    myVars = { :properties => node['zookeeper']['log4j'] }
+
+    template "#{zookeeper_conf_dir}/log4j.properties" do
+      source "generic.properties.erb"
+      mode "0644"
+      owner "root"
+      group "root"
+      action :create
+      variables myVars
+    end
+  end
+end # End log4j.properties
+
+# Update alternatives to point to our configuration
+execute "update zookeeper-conf alternatives" do
+  command "update-alternatives --install /etc/zookeeper/conf zookeeper-conf /etc/zookeeper/#{node['zookeeper']['conf_dir']} 50"
+  not_if "update-alternatives --display zookeeper-conf | grep best | awk '{print $5}' | grep /etc/zookeeper/#{node['zookeeper']['conf_dir']}"
+end
