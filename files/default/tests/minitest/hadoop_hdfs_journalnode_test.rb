2be171cc51e8699583a50f9f6e32c9b917729bd8
Chris Gianelloni <chris@continuuity.com>
2013-11-15 18:04:27 -0800

    Adding initial (empty) tests for the rest of the current recipes

---
+++ files/default/tests/minitest/hadoop_hdfs_journalnode_test.rb
@@ -0,0 +1,11 @@
+require File.expand_path('../support/helpers', __FILE__)
+
+describe 'hadoop::hadoop_hdfs_journalnode' do
+
+  include Helpers::Hadoop
+
+  # Example spec tests can be found at http://git.io/Fahwsw
+  it 'runs no tests by default' do
+  end
+
+end
