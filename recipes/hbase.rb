478b5f1832572f6654c5bc7d4d8f5532ea402262
Chris Gianelloni <chris@continuuity.com>
2013-11-20 16:01:12 -0800

    Add hbase_log_dir creation

--- recipes/hbase.rb
+++ recipes/hbase.rb
@@ -54,6 +54,17 @@ end # End hbase-policy.xml hbase-site.xml
 if node['hbase'].has_key? 'hbase_env'
   myVars = { :options => node['hbase']['hbase_env'] }
 
+  if node['hbase']['hbase_env'].has_key? 'hbase_log_dir'
+    node['hbase']['hbase_env']['hbase_log_dir'].each do |dir|
+      directory dir do
+        owner "hdfs"
+        group "hdfs"
+        mode "0755"
+        action :create
+      end
+    end
+  end
+
   template "#{hbase_conf_dir}/hbase-env.sh" do
     source "generic-env.sh.erb"
     mode "0755"
