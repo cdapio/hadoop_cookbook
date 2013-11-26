9b146a77114993d81634ae1eede0430d4da53566
Chris Gianelloni <chris@continuuity.com>
2013-11-26 13:06:59 -0800

    ZooKeeper requires both dataDir and clientPort to be set

--- attributes/zookeeper.rb
+++ attributes/zookeeper.rb
@@ -1,1 +1,3 @@
-default['zookeeper']['zoocfg']['dataDir'] = "/var/lib/zookeeper"
+# Minimal configuration: http://zookeeper.apache.org/doc/r3.4.5/zookeeperStarted.html#sc_InstallingSingleMode
+default['zookeeper']['zoocfg']['clientPort'] = '2181'
+default['zookeeper']['zoocfg']['dataDir'] = '/var/lib/zookeeper'
