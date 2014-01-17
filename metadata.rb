4220a5795a019b8dc9f7200d7c58cb2411a74427
Chris Gianelloni <chris@continuuity.com>
2014-01-16 18:21:25 -0800

    Version bumping to 0.1.0.900

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@continuuity.com'
 license          'Apache 2.0'
 description      'Installs/Configures hadoop'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.0.900'
 
 depends 'yum'
 depends 'apt'
