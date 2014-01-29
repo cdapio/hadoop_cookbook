cf9fa2258e0e85ae9fdb9681f9eff809aae017c1
Chris Gianelloni <chris@continuuity.com>
2014-01-28 18:51:11 -0800

    Remove yum_key LWRP calls and switch to new yum_repository format

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures hadoop'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
-depends 'yum'
+depends 'yum', '>= 3.0'
 depends 'apt'
