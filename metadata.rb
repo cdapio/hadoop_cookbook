021c3ae59296b262b463e63729e076f490b442e5
Chris Gianelloni <chris@continuuity.com>
2014-03-26 11:13:46 -0700

    Update description and version bump to 1.0.0 for release

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ name             'hadoop'
 maintainer       'Continuuity, Inc.'
 maintainer_email 'ops@continuuity.com'
 license          'Apache 2.0'
-description      'Installs/Configures hadoop'
+description      'Installs/Configures Hadoop (HDFS/YARN/MRv2), HBase, Hive, Oozie, Pig, and ZooKeeper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '1.0.0'
 
 depends 'yum', '>= 3.0'
 depends 'apt'
