2c2d67171203eea761d84acbe4560ddfdc4159a4
Chris Gianelloni <chris@continuuity.com>
2014-04-07 18:26:28 -0700

    Initial tests for default recipe, because tests are good, darn it.

--- files/default/tests/minitest/default_test.rb
+++ files/default/tests/minitest/default_test.rb
@@ -5,7 +5,35 @@ describe 'hadoop::default' do
   include Helpers::Hadoop
 
   # Example spec tests can be found at http://git.io/Fahwsw
-  it 'runs no tests by default' do
+  it 'creates hadoop conf dir' do
+    directory("/etc/hadoop/#{node['hadoop']['conf_dir']}")
+      .must_exist
+      .with(:owner, 'root')
+      .and(:group, 'root')
+      .and(:mode, '0755')
+  end
+
+  it 'ensures alternatives link' do
+    link('/etc/hadoop/conf')
+      .must_exist
+      .with(:link_type, :symbolic)
+      .and(:to, '/etc/alternatives/hadoop-conf')
+    link('/etc/alternatives/hadoop-conf')
+      .must_exist
+      .with(:link_type, :symbolic)
+      .and(:to, "/etc/hadoop/#{node['hadoop']['conf_dir']}")
+  end
+
+  it 'creates hadoop config files' do
+    %w(capacity_scheduler core_site hadoop_policy hdfs_site mapred_site yarn_site).each do |sitefile|
+      if node['hadoop'].has_key? sitefile
+        file("/etc/hadoop/#{node['hadoop']['conf_dir']}/#{sitefile.gsub('_','-')}.xml")
+          .must_exist
+          .with(:owner, 'root')
+          .and(:group, 'root')
+          .and(:mode, '0644')
+      end
+    end
   end
 
 end
