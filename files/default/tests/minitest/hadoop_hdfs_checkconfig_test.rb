76eb029d9c497e02a9de65bbaec9fff2ba64ad0c
Chris Gianelloni <chris@continuuity.com>
2014-04-09 14:35:39 -0700

    Fix datanode test to split on comma and create an array from the attribute

--- files/default/tests/minitest/hadoop_hdfs_checkconfig_test.rb
+++ files/default/tests/minitest/hadoop_hdfs_checkconfig_test.rb
@@ -3,9 +3,6 @@ require File.expand_path('../support/helpers', __FILE__)
 describe 'hadoop::hadoop_hdfs_checkconfig' do
 
   include Helpers::Hadoop
-
-  # Example spec tests can be found at http://git.io/Fahwsw
-  it 'runs no tests by default' do
-  end
+  # No tests, since this cookbook just tests attributes
 
 end
