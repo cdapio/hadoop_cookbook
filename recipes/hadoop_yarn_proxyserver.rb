a1f4a63e30869e07d9e0259018fe4b3a149d337e
Chris Gianelloni <chris@continuuity.com>
2014-04-10 11:38:56 -0700

    Adding support for YARN Web Proxy

---
+++ recipes/hadoop_yarn_proxyserver.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: hadoop
+# Recipe:: hadoop_yarn_proxyserver
+#
+# Copyright (C) 2013 Continuuity, Inc.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'hadoop::default'
+
+if (node['hadoop'].has_key? 'yarn_site' and node['hadoop']['yarn_site'].has_key? 'yarn.web-proxy.address')
+  Chef::Log.info("Setting up YARN Web Proxy at #{node['hadoop']['yarn_site']['yarn.web-proxy.address']}")
+else
+  Chef::Application.fatal!("YARN Web Proxy must be configured! Set default['hadoop']['yarn_site']['yarn.web-proxy.address']}!")
+end
+
+package "hadoop-yarn-proxyserver" do
+  action :install
+end
+
+service "hadoop-yarn-proxyserver" do
+  supports [ :restart => true, :reload => false, :status => true ]
+  action :nothing
+end
