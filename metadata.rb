b083821c70f76fa845f779df9867974989a13558
Chris Gianelloni <chris@continuuity.com>
2014-04-14 14:52:13 -0700

    Version bumping to 1.0.3 for release

--- metadata.rb
+++ metadata.rb
@@ -4,15 +4,15 @@ maintainer_email 'ops@continuuity.com'
 license          'Apache 2.0'
 description      'Installs/Configures Hadoop (HDFS/YARN/MRv2), HBase, Hive, Oozie, Pig, and ZooKeeper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.0.3'
 
 depends 'yum', '>= 3.0'
 depends 'apt'
 
-%w{ centos debian redhat scientific ubuntu }.each do |os|
+%w(centos debian redhat scientific ubuntu).each do |os|
   supports os
 end
 
-%w{ hadoop_cluster hadoop_cluster_rpm hadoop_for_hbase hbase hbase_cluster hive pig zookeeper zookeeper_cluster }.each do |cb|
+%w(hadoop_cluster hadoop_cluster_rpm hadoop_for_hbase hbase hbase_cluster hive pig zookeeper zookeeper_cluster).each do |cb|
   conflicts cb
 end
