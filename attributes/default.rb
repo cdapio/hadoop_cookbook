16704bcb51eab0e2035c6905cc992a6ff7d198b2
Chris Gianelloni <chris@continuuity.com>
2014-04-14 17:14:28 -0700

    Add spark['conf_dir']

--- attributes/default.rb
+++ attributes/default.rb
@@ -15,6 +15,7 @@ default['hadoop']['conf_dir'] = 'conf.chef'
 default['hbase']['conf_dir'] = node['hadoop']['conf_dir']
 default['hive']['conf_dir'] = node['hadoop']['conf_dir']
 default['oozie']['conf_dir'] = node['hadoop']['conf_dir']
+default['spark']['conf_dir'] = node['hadoop']['conf_dir']
 default['zookeeper']['conf_dir'] = node['hadoop']['conf_dir']
 
 ###
