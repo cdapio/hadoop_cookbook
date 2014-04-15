418931244c4f13937e903df3b69fb9dbdd5be787
Chris Gianelloni <chris@continuuity.com>
2014-04-14 17:27:12 -0700

    Attributes for spark to match the default spark-env.sh (as shipped with CDH5)

---
+++ attributes/spark.rb
@@ -0,0 +1,15 @@
+###
+# spark-env.sh
+###
+default['spark']['spark_env']['standalone_spark_master_host'] = node['fqdn']
+default['spark']['spark_env']['spark_master_ip'] = node['spark']['spark_env']['standalone_spark_master_host']
+# Run in JVM:
+default['spark']['spark_env']['spark_launch_with_scala'] = 0
+default['spark']['spark_env']['spark_library_path'] = '${SPARK_HOME}/lib'
+default['spark']['spark_env']['scala_library_path'] = '${SPARK_HOME}/lib'
+default['spark']['spark_env']['spark_master_webui_port'] = 18080
+default['spark']['spark_env']['spark_master_port'] = 7077
+default['spark']['spark_env']['spark_worker_port'] = 7078
+default['spark']['spark_env']['spark_worker_webui_port'] = 18081
+default['spark']['spark_env']['spark_worker_dir'] = '/var/run/spark/work'
+default['spark']['spark_env']['spark_log_dir'] = '/var/log/spark'
