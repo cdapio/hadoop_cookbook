835c23f63fd04e2c13b6922cd9b0c65637b95d27
Chris Gianelloni <chris@continuuity.com>
2014-04-23 16:33:28 -0700

    Stub out update-alternatives guard, add hdfs-site.xml attribute, test for directory creation, test for XML file creation from template, and verify templates are built with the correct contents

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -5,6 +5,8 @@ describe 'hadoop::default' do
     let(:chef_run) do
       ChefSpec::Runner.new(platform: 'centos', version: 6.4) do |node|
         node.automatic['domain'] = 'example.com'
+        node.default['hadoop']['hdfs_site']['dfs.datanode.max.xcievers'] = '4096'
+        stub_command('update-alternatives --display hadoop-conf | grep best | awk \'{print $5}\' | grep /etc/hadoop/conf.chef').and_return(false)
       end.converge(described_recipe)
     end
 
@@ -19,13 +21,35 @@ describe 'hadoop::default' do
       )
     end
 
-    it 'creates core-site.xml template' do
-      expect(chef_run).to create_template('/etc/hadoop/conf.chef/core-site.xml')
+    %w(hadoop-hdfs hadoop-mapreduce hadoop-yarn).each do |dir|
+      it "creates #{dir} directory" do
+        expect(chef_run).to create_directory("/tmp/#{dir}").with(
+          mode: '1777'
+        )
+      end
+    end
+
+    %w(capacity-scheduler.xml core-site.xml hdfs-site.xml yarn-site.xml).each do |xml|
+      it "creates #{xml} template" do
+        expect(chef_run).to create_template("/etc/hadoop/conf.chef/#{xml}")
+      end
+    end
+
+    it 'renders file capacity-scheduler.xml with yarn.scheduler.capacity.maximum-applications' do
+      expect(chef_run).to render_file('/etc/hadoop/conf.chef/capacity-scheduler.xml').with_content(
+        /yarn.scheduler.capacity.maximum-applications/
+      )
     end
 
     it 'renders file core-site.xml with fs.defaultFS' do
       expect(chef_run).to render_file('/etc/hadoop/conf.chef/core-site.xml').with_content(
         /fs.defaultFS/
+      )
+    end
+
+    it 'renders file hdfs-site.xml with dfs.datanode.max.xcievers' do
+      expect(chef_run).to render_file('/etc/hadoop/conf.chef/hdfs-site.xml').with_content(
+        /dfs.datanode.max.xcievers/
       )
     end
   end
