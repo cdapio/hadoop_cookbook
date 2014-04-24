bb55cc033aed7639a6eb3561fcc830cd88062b5e
Chris Gianelloni <chris@continuuity.com>
2014-04-24 12:58:37 -0700

    Copy HBase tests to Hive

--- files/default/tests/minitest/hive_test.rb
+++ files/default/tests/minitest/hive_test.rb
@@ -5,5 +5,35 @@ describe 'hadoop::hive' do
   include Helpers::Hadoop
 
   # Example spec tests can be found at http://git.io/Fahwsw
+  it 'creates hive conf dir' do
+    directory("/etc/hive/#{node['hive']['conf_dir']}")
+      .must_exist
+      .with(:owner, 'root')
+      .and(:group, 'root')
+      .and(:mode, '0755')
+  end
+
+  it 'ensures alternatives link' do
+    link('/etc/hive/conf')
+      .must_exist
+      .with(:link_type, :symbolic)
+      .and(:to, '/etc/alternatives/hive-conf')
+    link('/etc/alternatives/hive-conf')
+      .must_exist
+      .with(:link_type, :symbolic)
+      .and(:to, "/etc/hive/#{node['hive']['conf_dir']}")
+  end
+
+  it 'creates hive config files' do
+    %w(hive_site).each do |sitefile|
+      if node['hive'].key? sitefile
+        file("/etc/hive/#{node['hive']['conf_dir']}/#{sitefile.gsub('_', '-')}.xml")
+          .must_exist
+          .with(:owner, 'root')
+          .and(:group, 'root')
+          .and(:mode, '0644')
+      end
+    end
+  end
 
 end
