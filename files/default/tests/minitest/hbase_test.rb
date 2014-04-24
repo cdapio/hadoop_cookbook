24b4615030c03e4e3cc02ac8e90e93b388d50897
Chris Gianelloni <chris@continuuity.com>
2014-04-24 12:56:51 -0700

    Add tests for hbase recipe

--- files/default/tests/minitest/hbase_test.rb
+++ files/default/tests/minitest/hbase_test.rb
@@ -5,5 +5,35 @@ describe 'hadoop::hbase' do
   include Helpers::Hadoop
 
   # Example spec tests can be found at http://git.io/Fahwsw
+  it 'creates hbase conf dir' do
+    directory("/etc/hbase/#{node['hbase']['conf_dir']}")
+      .must_exist
+      .with(:owner, 'root')
+      .and(:group, 'root')
+      .and(:mode, '0755')
+  end
+
+  it 'ensures alternatives link' do
+    link('/etc/hbase/conf')
+      .must_exist
+      .with(:link_type, :symbolic)
+      .and(:to, '/etc/alternatives/hbase-conf')
+    link('/etc/alternatives/hbase-conf')
+      .must_exist
+      .with(:link_type, :symbolic)
+      .and(:to, "/etc/hbase/#{node['hbase']['conf_dir']}")
+  end
+
+  it 'creates hbase config files' do
+    %w(hbase_policy hbase_site).each do |sitefile|
+      if node['hbase'].key? sitefile
+        file("/etc/hbase/#{node['hbase']['conf_dir']}/#{sitefile.gsub('_', '-')}.xml")
+          .must_exist
+          .with(:owner, 'root')
+          .and(:group, 'root')
+          .and(:mode, '0644')
+      end
+    end
+  end
 
 end
