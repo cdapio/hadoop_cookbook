74c871f9de719c1007cf247c4893365bd2bc5b7b
Chris Gianelloni <chris@continuuity.com>
2014-04-25 16:27:07 -0700

    Version bumping to 1.0.7 for release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@continuuity.com'
 license          'Apache 2.0'
 description      'Installs/Configures Hadoop (HDFS/YARN/MRv2), HBase, Hive, Oozie, Pig, and ZooKeeper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.6'
+version          '1.0.7'
 
 depends 'yum', '>= 3.0'
 depends 'apt'
