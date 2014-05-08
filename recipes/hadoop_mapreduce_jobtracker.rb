645bea08f0d1468ad1c145e8d8fc560e3ce33272
Chris Gianelloni <chris@continuuity.com>
2014-05-08 12:05:15 -0700

    First stab at adding JobTracker/TaskTracker support. This is failing chefspec and I am not sure why. The package is available on CentOS 6.4 using hadoop['distribution'] == 'cdh' in Vagrant.

---
+++ recipes/hadoop_mapreduce_jobtracker.rb
@@ -0,0 +1,39 @@
+#
+# Cookbook Name:: hadoop
+# Recipe:: hadoop_mapreduce_jobtracker
+#
+# Copyright (C) 2013-2014 Continuuity, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'hadoop::default'
+
+# Only CDH supports a JobTracker package
+# if node['hadoop']['distribution'] == 'cdh'
+#  Chef::Log.warn('Cloudera provides a compatibility JobTracker, you should consider switching to YARN (MRv2)!')
+
+package 'hadoop-0.20-mapreduce-jobtracker' do
+  action :install
+  only_if { node['hadoop']['distribution'] == 'cdh' }
+end
+
+service 'hadoop-0.20-mapreduce-jobtracker' do
+  supports [:restart => true, :reload => false, :status => true]
+  action :nothing
+  only_if { node['hadoop']['distribution'] == 'cdh' }
+end
+
+# else
+#  Chef::Log.warn('This distribution does not support a JobTracker, use YARN (MRv2) instead!')
+# end
