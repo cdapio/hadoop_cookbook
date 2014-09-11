9914cffd3ebb19b49f3a8d7d35f925e8425ba0f1
Chris Gianelloni <chris@continuuity.com>
2014-09-11 11:00:25 -0700

    Initial hive specs

---
+++ spec/hive_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'hadoop::hive' do
+  context 'on Centos 6.4 x86_64' do
+    let(:chef_run) do
+      ChefSpec::Runner.new(platform: 'centos', version: 6.4) do |node|
+        node.automatic['domain'] = 'example.com'
+        stub_command('update-alternatives --display hadoop-conf | grep best | awk \'{print $5}\' | grep /etc/hadoop/conf.chef').and_return(false)
+        stub_command('update-alternatives --display hive-conf | grep best | awk \'{print $5}\' | grep /etc/hive/conf.chef').and_return(false)
+      end.converge(described_recipe)
+    end
+
+    it 'install hive package' do
+      expect(chef_run).to install_package('hive')
+    end
+  end
+end
