36cac11960be6177aa6031dad621a749971a327e
Chris Gianelloni <chris@continuuity.com>
2014-09-11 11:51:04 -0700

    Add tests for mysql-connector-java and postgresql-jdbc packages
    ChefSpec Coverage report generated...
    
      Total Resources:   106
      Touched Resources: 85
      Touch Coverage:    80.19%

--- spec/hive_spec.rb
+++ spec/hive_spec.rb
@@ -5,13 +5,17 @@ describe 'hadoop::hive' do
     let(:chef_run) do
       ChefSpec::Runner.new(platform: 'centos', version: 6.4) do |node|
         node.automatic['domain'] = 'example.com'
+        node.default['hive']['hive_site']['hive.exec.local.scratchdir'] = '/tmp'
         stub_command('update-alternatives --display hadoop-conf | grep best | awk \'{print $5}\' | grep /etc/hadoop/conf.chef').and_return(false)
         stub_command('update-alternatives --display hive-conf | grep best | awk \'{print $5}\' | grep /etc/hive/conf.chef').and_return(false)
       end.converge(described_recipe)
     end
 
-    it 'install hive package' do
-      expect(chef_run).to install_package('hive')
+    %w(mysql-connector-java postgresql-jdbc hive).each do |pkg|
+      it "install #{pkg} package" do
+        expect(chef_run).to install_package(pkg)
+      end
     end
+
   end
 end
