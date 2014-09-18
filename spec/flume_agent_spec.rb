399915bdc2530d2034529bab619ce0e0fe327c29
Chris Gianelloni <chris@continuuity.com>
2014-09-18 15:59:51 -0700

    Initial tests for flume

---
+++ spec/flume_agent_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe 'hadoop::flume_agent' do
+  context 'on Centos 6.4 x86_64' do
+    let(:chef_run) do
+      ChefSpec::Runner.new(platform: 'centos', version: 6.4) do |node|
+        node.automatic['domain'] = 'example.com'
+      end.converge(described_recipe)
+    end
+
+    it 'install flume-agent package' do
+      expect(chef_run).to install_package('flume-agent')
+    end
+
+    it 'creates flume-agent service resource, but does not run it' do
+      expect(chef_run).to_not start_service('flume-agent')
+    end
+  end
+
+  context 'using CDH' do
+    let(:chef_run) do
+      ChefSpec::Runner.new(platform: 'centos', version: 6.4) do |node|
+        node.automatic['domain'] = 'example.com'
+        node.override['hadoop']['distribution'] = 'cdh'
+      end.converge(described_recipe)
+    end
+
+    it 'install flume-ng package' do
+      expect(chef_run).to install_package('flume-ng-agent')
+    end
+  end
+end
