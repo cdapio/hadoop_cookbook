6574c8cb112d6eae7803f57879dd291b53382017
Chris Gianelloni <chris@cask.co>
2014-11-03 18:54:14 -0800

    Improve Hive tests...
    ChefSpec Coverage report generated...
    
      Total Resources:   133
      Touched Resources: 132
      Touch Coverage:    99.25%

--- spec/hive_spec.rb
+++ spec/hive_spec.rb
@@ -5,7 +5,7 @@ describe 'hadoop::hive' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'centos', version: 6.5) do |node|
         node.automatic['domain'] = 'example.com'
-        node.default['hive']['hive_site']['hive.exec.local.scratchdir'] = '/tmp'
+        node.default['hive']['hive_site']['hive.exec.local.scratchdir'] = '/tmp/hive/scratch'
         node.default['hive']['hive_env']['hive_log_dir'] = '/data/log/hive'
         stub_command('update-alternatives --display hadoop-conf | grep best | awk \'{print $5}\' | grep /etc/hadoop/conf.chef').and_return(false)
         stub_command('update-alternatives --display hive-conf | grep best | awk \'{print $5}\' | grep /etc/hive/conf.chef').and_return(false)
@@ -35,16 +35,47 @@ describe 'hadoop::hive' do
     it 'does not execute execute[hive-hdfs-homedir]' do
       expect(chef_run).not_to run_execute('hive-hdfs-homedir')
     end
+
     it 'executes execute[update hive-conf alternatives]' do
       expect(chef_run).to run_execute('update hive-conf alternatives')
     end
 
+    it 'creates hive HIVE_LOG_DIR' do
+      expect(chef_run).to create_directory('/data/log/hive').with(
+        mode: '0755',
+        user: 'hive',
+        group: 'hive'
+      )
+    end
+
+    it 'deletes /var/log/hive' do
+      expect(chef_run).to delete_directory('/var/log/hive')
+    end
+
+    it 'creates /var/log/hive symlink' do
+      link = chef_run.link('/var/log/hive')
+      expect(link).to link_to('/data/log/hive')
+    end
+
+    %w(
+      /etc/hive/conf.chef/hive-site.xml
+      /etc/hive/conf.chef/hive-env.sh
+    ).each do |template|
+      it "creates #{template} template" do
+        expect(chef_run).to create_template(template)
+      end
+    end
+
+    it 'creates /tmp/hive/scratch directory' do
+      expect(chef_run).to create_directory('/tmp/hive/scratch')
+    end
+
   end
+
   context 'on Ubuntu 12.04' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'ubuntu', version: 12.04) do |node|
         node.automatic['domain'] = 'example.com'
-        node.default['hive']['hive_site']['hive.exec.local.scratchdir'] = '/tmp'
         stub_command('update-alternatives --display hadoop-conf | grep best | awk \'{print $5}\' | grep /etc/hadoop/conf.chef').and_return(false)
         stub_command('update-alternatives --display hive-conf | grep best | awk \'{print $5}\' | grep /etc/hive/conf.chef').and_return(false)
       end.converge(described_recipe)
