0aa8ec9c2d0694df345cc8ff41d90ad9c10fe20d
Chris Gianelloni <chris@cask.co>
2014-11-05 15:20:55 -0800

    Stub command for Oozie, too

--- spec/oozie_spec.rb
+++ spec/oozie_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 describe 'hadoop::oozie' do
   context 'on Centos 6.5 x86_64' do
     let(:chef_run) do
-      ChefSpec::Runner.new(platform: 'centos', version: 6.5) do |node|
+      ChefSpec::SoloRunner.new(platform: 'centos', version: 6.5) do |node|
         node.automatic['domain'] = 'example.com'
         node.default['oozie']['oozie_env']['oozie_log_dir'] = '/data/log/oozie'
+        stub_command('test -L /var/log/oozie').and_return(false)
         stub_command('update-alternatives --display oozie-conf | grep best | awk \'{print $5}\' | grep /etc/oozie/conf.chef').and_return(false)
       end.converge(described_recipe)
     end
