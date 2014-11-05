95b0f5816c558ddbce895615aad7f2f4876b85b8
Chris Gianelloni <chris@cask.co>
2014-11-05 11:27:50 -0800

    Initial Spark chefspec tests

---
+++ spec/spark_historyserver_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe 'hadoop::spark_historyserver' do
+  context 'on Centos 6.5 x86_64' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'centos', version: 6.5) do |node|
+        node.automatic['domain'] = 'example.com'
+        node.default['spark']['release']['install'] = true
+        stub_command('update-alternatives --display spark-conf | grep best | awk \'{print $5}\' | grep /etc/spark/conf.chef').and_return(false)
+      end.converge(described_recipe)
+    end
+
+    it 'does not install spark-history-server package' do
+      expect(chef_run).not_to install_package('spark-history-server')
+    end
+  end
+
+  context 'using CDH 5' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'centos', version: 6.5) do |node|
+        node.automatic['domain'] = 'example.com'
+        node.override['hadoop']['distribution'] = 'cdh'
+        node.override['hadoop']['distribution_version'] = 5
+        stub_command('update-alternatives --display spark-conf | grep best | awk \'{print $5}\' | grep /etc/spark/conf.chef').and_return(false)
+      end.converge(described_recipe)
+    end
+
+    it 'installs spark-history-server package' do
+      expect(chef_run).to install_package('spark-history-server')
+    end
+  end
+end
