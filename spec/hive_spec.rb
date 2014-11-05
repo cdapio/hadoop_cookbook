e35dcf89344bda2a5f0c884c6bfbd82c15cbd8a3
Chris Gianelloni <chris@cask.co>
2014-11-05 15:15:43 -0800

    Stub command for tests

--- spec/hive_spec.rb
+++ spec/hive_spec.rb
@@ -7,6 +7,7 @@ describe 'hadoop::hive' do
         node.automatic['domain'] = 'example.com'
         node.default['hive']['hive_site']['hive.exec.local.scratchdir'] = '/tmp/hive/scratch'
         node.default['hive']['hive_env']['hive_log_dir'] = '/data/log/hive'
+        stub_command('test -L /var/log/hive').and_return(false)
         stub_command('update-alternatives --display hadoop-conf | grep best | awk \'{print $5}\' | grep /etc/hadoop/conf.chef').and_return(false)
         stub_command('update-alternatives --display hive-conf | grep best | awk \'{print $5}\' | grep /etc/hive/conf.chef').and_return(false)
       end.converge(described_recipe)
