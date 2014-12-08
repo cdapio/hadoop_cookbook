11d3b67b332c6a626f1c24cd01cd442b0f78d182
Michael Andrews <michael@socialcast.com>
2014-12-08 14:09:39 -0800

    Remove duplicate attribute initialization

--- attributes/tez.rb
+++ attributes/tez.rb
@@ -1,5 +1,3 @@
-default['tez']['conf_dir'] = node['hadoop']['conf_dir']
-
 default['tez']['tez_site']['tez.lib.uris'] = '${fs.default.name}/apps/tez/,${fs.default.name}/apps/tez/lib/'
 default['tez']['tez_env']['tez_conf_dir'] = "/etc/tez/#{node['tez']['conf_dir']}"
 default['tez']['tez_env']['tez_jars'] = '/usr/lib/tez/*:/usr/lib/tez/lib/*'
