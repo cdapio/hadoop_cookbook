e201701b9ad69cf6fcc1428cb12ceaefc3aa0bed
Chris Gianelloni <chris@cask.co>
2015-01-14 14:43:33 -0800

    COOK-19 Configure hive.metastore.uris

--- attributes/hive.rb
+++ attributes/hive.rb
@@ -1,2 +1,3 @@
+default['hive']['hive_site']['hive.metastore.uris'] = 'thrift://localhost:9083'
 default['hive']['hive_site']['javax.jdo.option.ConnectionURL'] = 'jdbc:derby:;databaseName=/var/lib/hive/metastore/metastore_db;create=true'
 default['hive']['hive_site']['javax.jdo.option.ConnectionDriverName'] = 'org.apache.derby.jdbc.EmbeddedDriver'
