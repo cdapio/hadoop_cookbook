cfa5a16d5bf42204ffa20099039c8c190ec7e008
Joe DeCello <taverentech@gmail.com>
2015-04-03 18:08:03 -0700

    Update _system_tuning.rb

--- recipes/_system_tuning.rb
+++ recipes/_system_tuning.rb
@@ -33,7 +33,7 @@ end
 # Do not assume file exists, Ubuntu 14 in AWS does not have thp_defrag file
 update_thp_defrag = true
 if ::File.exist?(thp_defrag)
-  file = File.new("#{thp_defrag}")
+  file = File.new(thp_defrag)
   text = file.read
   update_thp_defrag = false if text =~ /\[never\]/
 else
