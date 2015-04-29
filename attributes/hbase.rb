5b7300308d9bffc08eee771d4ead242ad5e70a23
Chris Gianelloni <chris@cask.co>
2015-04-29 14:16:51 -0700

    Set limits for HBase

--- attributes/hbase.rb
+++ attributes/hbase.rb
@@ -4,3 +4,6 @@ default['hbase']['hbase_site']['hbase.rootdir'] = "#{node['hadoop']['core_site']
 default['hbase']['hbase_policy']['security.client.protocol.acl'] = '*'
 default['hbase']['hbase_policy']['security.admin.protocol.acl'] = '*'
 default['hbase']['hbase_policy']['security.masterregion.protocol.acl'] = '*'
+# limits.d settings
+default['hbase']['limits']['nofile'] = '32768'
+default['hbase']['limits']['memlock'] = 'unlimited'
