d866879e256e802befe983c83d955f8c38adcd82
Chris Gianelloni <chris@cask.co>
2015-05-14 18:13:03 -0700

    Remove redundant checks

--- spec/hive_spec.rb
+++ spec/hive_spec.rb
@@ -61,50 +61,14 @@ describe 'hadoop::hive' do
     end
   end
 
-  context 'using HDP 2.2' do
+  context 'using default hive.exec.local.scratchdir' do
     let(:chef_run) do
-      ChefSpec::SoloRunner.new(platform: 'centos', version: 6.5) do |node|
-        node.override['hadoop']['distribution'] = 'hdp'
-        node.override['hadoop']['distribution_version'] = '2.2.4.2'
+      ChefSpec::SoloRunner.new(platform: 'centos', version: 6.6) do |node|
         node.automatic['domain'] = 'example.com'
-        node.default['hive']['hive_site']['hive.exec.local.scratchdir'] = '/tmp/hive/scratch'
         node.default['hive']['hive_env']['hive_log_dir'] = '/data/log/hive'
         stub_command('test -L /var/log/hive').and_return(false)
         stub_command('update-alternatives --display hive-conf | grep best | awk \'{print $5}\' | grep /etc/hive/conf.chef').and_return(false)
       end.converge(described_recipe)
-    end
-
-    %w(mysql-connector-java postgresql-jdbc).each do |pkg|
-      it "link #{pkg}.jar" do
-        link = chef_run.link("/usr/hdp/current/hive-client/lib/#{pkg}.jar")
-        expect(link).to link_to("/usr/share/java/#{pkg}.jar")
-      end
-    end
-  end
-
-  context 'on Ubuntu 12.04' do
-    let(:chef_run) do
-      ChefSpec::SoloRunner.new(platform: 'ubuntu', version: 12.04) do |node|
-        node.automatic['domain'] = 'example.com'
-        stub_command('update-alternatives --display hadoop-conf | grep best | awk \'{print $5}\' | grep /etc/hadoop/conf.chef').and_return(false)
-        stub_command('update-alternatives --display hive-conf | grep best | awk \'{print $5}\' | grep /etc/hive/conf.chef').and_return(false)
-      end.converge(described_recipe)
-    end
-
-    %w(mysql-connector-java libpostgresql-jdbc-java).each do |pkg|
-      it "install #{pkg} package" do
-        expect(chef_run).to install_package(pkg)
-      end
-    end
-    %w(mysql-connector-java postgresql-jdbc4).each do |jar|
-      it "link #{jar}.jar" do
-        link = chef_run.link("/usr/lib/hive/lib/#{jar}.jar")
-        expect(link).to link_to("/usr/share/java/#{jar}.jar")
-      end
-    end
-
-    it 'installs hive package' do
-      expect(chef_run).to install_package('hive')
     end
 
     it 'does not create /tmp/hive directory' do
@@ -112,25 +76,15 @@ describe 'hadoop::hive' do
     end
   end
 
-  context 'using 12.04 HDP 2.2' do
+  context 'using /tmp/hive for hive.exec.local.scratchdir' do
     let(:chef_run) do
-      ChefSpec::SoloRunner.new(platform: 'ubuntu', version: 12.04) do |node|
-        node.override['hadoop']['distribution'] = 'hdp'
-        node.override['hadoop']['distribution_version'] = '2.2'
-        node.default['hive']['hive_env']['hive_log_dir'] = '/data/log/hive'
+      ChefSpec::SoloRunner.new(platform: 'centos', version: 6.6) do |node|
         node.default['hive']['hive_site']['hive.exec.local.scratchdir'] = '/tmp/hive'
         node.automatic['domain'] = 'example.com'
         stub_command(/test -L /).and_return(false)
         stub_command('update-alternatives --display hadoop-conf | grep best | awk \'{print $5}\' | grep /etc/hadoop/conf.chef').and_return(false)
         stub_command('update-alternatives --display hive-conf | grep best | awk \'{print $5}\' | grep /etc/hive/conf.chef').and_return(false)
       end.converge(described_recipe)
-    end
-
-    %w(mysql-connector-java postgresql-jdbc4).each do |jar|
-      it "link #{jar}.jar" do
-        link = chef_run.link("/usr/hdp/current/hive-client/lib/#{jar}.jar")
-        expect(link).to link_to("/usr/share/java/#{jar}.jar")
-      end
     end
 
     it 'creates /tmp/hive directory' do
