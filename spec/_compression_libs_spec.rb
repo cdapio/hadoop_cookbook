82d22d70208cffd82b38f7d99d20822678a37c42
Chris Gianelloni <chris@cask.co>
2015-05-20 16:06:06 -0700

    Update tests to match intent of changed code

--- spec/_compression_libs_spec.rb
+++ spec/_compression_libs_spec.rb
@@ -6,6 +6,27 @@ describe 'hadoop::_compression_libs' do
       ChefSpec::SoloRunner.new(platform: 'centos', version: 6.6).converge(described_recipe)
     end
 
+    %w(snappy snappy-devel).each do |pkg|
+      it "installs #{pkg} package" do
+        expect(chef_run).to install_package(pkg)
+      end
+    end
+
+    %w(lzo lzo-devel hadooplzo hadooplzo-native).each do |pkg|
+      it "does not install #{pkg} package" do
+        expect(chef_run).not_to install_package(pkg)
+      end
+    end
+  end
+
+  context 'using HDP 2.2' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'centos', version: 6.6) do |node|
+        node.override['hadoop']['distribution'] = 'hdp'
+        node.override['hadoop']['distribution_version'] = '2.2.4.2'
+      end.converge(described_recipe)
+    end
+
     %w(snappy snappy-devel lzo lzo-devel hadooplzo hadooplzo-native).each do |pkg|
       it "installs #{pkg} package" do
         expect(chef_run).to install_package(pkg)
@@ -37,6 +58,27 @@ describe 'hadoop::_compression_libs' do
   context 'on Ubuntu 12.04' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'ubuntu', version: 12.04).converge(described_recipe)
+    end
+
+    %w(libsnappy1 libsnappy1-dev).each do |pkg|
+      it "installs #{pkg} package" do
+        expect(chef_run).to install_package(pkg)
+      end
+    end
+
+    %w(liblzo2-2 liblzo2-dev hadooplzo).each do |pkg|
+      it "does not install #{pkg} package" do
+        expect(chef_run).not_to install_package(pkg)
+      end
+    end
+  end
+
+  context 'using HDP 2.2' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'ubuntu', version: 12.04) do |node|
+        node.override['hadoop']['distribution'] = 'hdp'
+        node.override['hadoop']['distribution_version'] = '2.2.4.2'
+      end.converge(described_recipe)
     end
 
     %w(libsnappy1 libsnappy1-dev liblzo2-2 liblzo2-dev hadooplzo).each do |pkg|
