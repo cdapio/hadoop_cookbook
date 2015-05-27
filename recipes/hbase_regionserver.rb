a3b6994f67c4500bc3e0262d172e91a6824e723d
Chris Gianelloni <chris@cask.co>
2015-05-26 19:23:20 -0700

    Update HBase to use new init scripts

--- recipes/hbase_regionserver.rb
+++ recipes/hbase_regionserver.rb
@@ -22,6 +22,10 @@ include_recipe 'hadoop::_hbase_checkconfig'
 include_recipe 'hadoop::_system_tuning'
 pkg = 'hbase-regionserver'
 
+# Load helpers
+Chef::Recipe.send(:include, Hadoop::Helpers)
+Chef::Resource::Template.send(:include, Hadoop::Helpers)
+
 package pkg do
   action :nothing
 end
@@ -39,9 +43,49 @@ ruby_block "package-#{pkg}" do
   end
 end
 
+hbase_log_dir =
+  if node['hbase'].key?('hbase_env') && node['hbase']['hbase_env'].key?('hbase_log_dir')
+    node['hbase']['hbase_env']['hbase_log_dir']
+  else
+    '/var/log/hbase'
+  end
+
+# Create /etc/default configuration
+template "/etc/default/#{pkg}" do
+  source 'generic-env.sh.erb'
+  mode '0755'
+  owner 'root'
+  group 'root'
+  action :create
+  variables :options => {
+    'hbase_home' => "#{lib_dir}/hbase",
+    'hbase_pid_dir' => '/var/run/hbase',
+    'hbase_log_dir' => hbase_log_dir,
+    'hbase_ident_string' => 'hbase'
+  }
+end
+
+template "/etc/init.d/#{pkg}" do
+  source 'hadoop-init.erb'
+  mode '0755'
+  owner 'root'
+  group 'root'
+  action :create
+  variables :options => {
+    'desc' => 'HBase RegionServer',
+    'name' => pkg,
+    'process' => 'java',
+    'binary' => "#{lib_dir}/hbase/bin/hbase-daemon.sh",
+    'args' => '--config /etc/hbase/conf start regionserver',
+    'user' => 'hbase',
+    'home' => "#{lib_dir}/hbase",
+    'pidfile' => "${HBASE_PID_DIR}/#{pkg}.pid",
+    'logfile' => "${HBASE_LOG_DIR}/#{pkg}.log"
+  }
+end
+
 service pkg do
+  status_command "service #{pkg} status"
   supports [:restart => true, :reload => false, :status => true]
-  # cdh4.4 init scripts do not return non-zero exit codes for status
-  status_command "service #{pkg} status | grep -v 'not running'"
   action :nothing
 end
