ff96540f74ec2ca6aeb5d3970aa6d7ff79c46e37
Chris Gianelloni <chris@cask.co>
2016-06-20 13:28:38 -0700

    Version bump to 2.3.3 (#264)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@cask.co'
 license          'Apache 2.0'
 description      'Installs/Configures Hadoop (HDFS/YARN/MRv2), HBase, Hive, Flume, Oozie, Pig, Spark, Storm, Tez, and ZooKeeper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.3.2'
+version          '2.3.3'
 
 depends 'yum', '>= 3.0'
 depends 'apt', '>= 2.1.2'
@@ -15,10 +15,41 @@ end
 
 recommends 'java', '~> 1.21'
 
-%w(amazon centos debian redhat scientific ubuntu).each do |os|
+%w(
+  amazon
+  centos
+  debian
+  redhat
+  scientific
+  ubuntu
+).each do |os|
   supports os
 end
 
-%w(hadoop_cluster hadoop_cluster_rpm hadoop_for_hbase hbase hbase_cluster hive pig spark storm storm-cluster tez zookeeper zookeeper_cluster).each do |cb|
+%w(
+  apache_hadoop
+  apache_spark
+  apache_zookeeper
+  hadoop_cluster
+  hadoop_cluster_rpm
+  hadoop_for_hbase
+  hadoop_spark
+  hbase
+  hbase_cluster
+  hive
+  pig
+  spark
+  spark-jobserver
+  spark-platform
+  storm
+  storm-cluster
+  storm-platform
+  tez
+  zookeeper
+  zookeeperd
+  zookeeper_cluster
+  zookeeper-cluster
+  zookeeper-platform
+).each do |cb|
   conflicts cb
 end
