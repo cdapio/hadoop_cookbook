5670fff1cd2ce037579fe08b40e1f739cf501a36
Chris Gianelloni <chris@cask.co>
2016-09-09 18:03:07 -0400

    Create /etc/profile.d/hadoop.sh, per @pauloricardomg

--- recipes/default.rb
+++ recipes/default.rb
@@ -290,3 +290,11 @@ execute 'update hadoop-conf alternatives' do
   command "update-alternatives --install /etc/hadoop/conf hadoop-conf /etc/hadoop/#{node['hadoop']['conf_dir']} 50"
   not_if "update-alternatives --display hadoop-conf | grep best | awk '{print $5}' | grep /etc/hadoop/#{node['hadoop']['conf_dir']}"
 end
+
+# Export hadoop environment variables
+template "/etc/profile.d/hadoop.sh" do
+  source "hadoop-profile.erb"
+  mode 0755
+  owner "root"
+  group "root"
+end
